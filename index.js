7f4057c59250d65c98e0789e62d79fd47e6d4a84
Ihewaz <ihewaz@mail.ru>
2017-03-29 23:38:08 +0300

    тестирую знания es6  по кусочкам
    телега тут как альтернатива консоли - более красиво и разнообразие
    радует

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-var TelegramBot = require('node-telegram-bot-api');
-var token = '361664269:AAG-xCg5Y0E6mlyAlMVSbXi-CJDVie-25kw';
-var bot = new TelegramBot(token, {
+const TelegramBot = require('node-telegram-bot-api');
+const token = '361664269:AAG-xCg5Y0E6mlyAlMVSbXi-CJDVie-25kw';
+const bot = new TelegramBot(token, {
     polling: true
 });
 
+
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Received ${msg.chat.username}'s message ${binarificate(msg.text)}, it's in binary because I want my bot to do anything)`);
+    let chatId = msg.chat.id;
+    bot.sendMessage(chatId, changeTwo(...msg.text.split(' ')));
 });
 
-var binarificate = function(strIn) {
-    var output = "";
-    for (var i = 0; i < strIn.length; i++) {
-        output += strIn[i].charCodeAt(0).toString(2) + " ";
-        console.log(output);
+const duplicate = (input) => {
+    return input.toString().toUpperCase() + input;
+    //работает
+}
+
+const changeTwo = (...par) => {
+    //changeTwo(...msg.text.split(' '))
+    //для вызова используем спреад оператор
+    return par.reverse().join(' ');
+    //меняет порядок слов
+}
+
+const sendGreatPic = () => {
+    let ranToFour = Math.floor(Math.random * 4);
+}
+
+const howForOfDiffersFromForIN = () => {
+    const someGuy = {
+        name: "Ivan",
+        work: "JS development",
+        hobby: "kickboxing"
     }
-    return output;
+    const colors = ['blue', 'red', 'green', 'pink'];
+    //for in идет по маркерам доступа (индексы/свойства)
+    //for of по их значенниям
+    let outForOf = " ",
+        outForIn = " ";
+    for (propName in someGuy) {
+        outForIn += someGuy[propName] + " ";
+    }
+    for (propVal of colors) {
+        outForOf += propVal + " ";
+    }
+    if (outForOf != outForIn) {
+        return `методы возвращают одно и то же - я воспользовался obj[propName] для доступа к значениям в цикле for..in
+        ${outForOf} и ${outForIn} - результаты циклов по объекту и массиву`;
+    }
 }
